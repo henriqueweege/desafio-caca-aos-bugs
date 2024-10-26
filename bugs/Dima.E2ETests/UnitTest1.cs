1046b2eb558111f065c7031c0e8220a03270ab3e
henriqueweege <henriqueweege@gmail.com>
2024-10-26 20:44:22 -0300

    implementing E2E tests

---
+++ bugs/Dima.E2ETests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics;
+
+namespace Dima.E2ETests
+{
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        public UnitTest1()
+        {
+            RunWebProject().Wait();
+
+            driver = (IWebDriver)new ChromeDriver();
+        }
+        private static async Task RunWebProject()
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/K " + "dotnet run --project ../../../../Dima.Web/Dima.Web.csproj");
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = true;
+
+            process = Process.Start(processInfo);
+
+            await Task.Delay(20000);
+        }
+
+        [Fact]
+        public async Task ValidateTheMessageIsDisplayed()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5028/comecar");
+            await Task.Delay(5000);
+            driver.FindElement(By.Id("e-mail")).SendKeys("teste@mail.com");
+            driver.FindElement(By.Id("password")).SendKeys("xpto");
+            driver.FindElement(By.Id("submit")).Click();
+            await Task.Delay(2000);
+            Assert.Equal(driver.Url, "http://localhost:5028/comecar");
+        }
+
+    }
+}
--- unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
+++ unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
@@ -1,20 +1,13 @@
 using Balta.Domain.SharedContext.Extensions;
 using FluentAssertions;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Balta.Domain.Test.SharedContext.Extensions;
 
 public class StringExtensionsTests
 {
-    [Theory]
-    [InlineData("xpto")]
-    [InlineData("XPTO")]
-    [InlineData("e6697c6d-fe4b-45dc-a0ad-5e3db3c76bd3")]
-    [InlineData("e6697?!@-fe4b-4?!@c-?!@ad-5e3db3c?!@d3")]
-    [InlineData("E6697C6D-FE4B-45DC-A0AD-5E3DB3C76BD3")]
-    [InlineData("E?!@97C6D-F?!@B-45?!@-A?!@D-5?!@DB?!@76BD3")]
-    public void GivenString_ToBase64_ShouldConvertCorrectly(string toConvert)//ShouldGenerateBase64FromString
+    [Theory(DisplayName = "ShouldGenerateBase64FromString"), MemberData(nameof(GetStringToConvert))]
+    public void GivenString_ToBase64_ShouldConvertCorrectly(string toConvert)
     {
         // Arrange
         var expected = Convert.ToBase64String(Encoding.ASCII.GetBytes(toConvert));
@@ -25,5 +18,15 @@ public class StringExtensionsTests
         // Assert
         converted.Should().Be(expected);
 
+    }
+
+    public static IEnumerable<object[]?> GetStringToConvert()
+    {
+        yield return new object[] { "xpto" };
+        yield return new object[] { "XPTO" };
+        yield return new object[] { "e6697c6d-fe4b-45dc-a0ad-5e3db3c76bd3" };
+        yield return new object[] { "e6697?!@-fe4b-4?!@c-?!@ad-5e3db3c?!@d3" };
+        yield return new object[] { "E6697C6D-FE4B-45DC-A0AD-5E3DB3C76BD3" };
+        yield return new object[] { "E?!@97C6D-F?!@B-45?!@-A?!@D-5?!@DB?!@76BD3" };
     }
 }
