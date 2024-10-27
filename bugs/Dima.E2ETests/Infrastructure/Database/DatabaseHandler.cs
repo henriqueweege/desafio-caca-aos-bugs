bc092ce546ba476bfa3d9fb8b2ecdded3829b0f9
henriqueweege <henriqueweege@gmail.com>
2024-10-27 19:56:21 -0300

    setting sql image

--- bugs/Dima.E2ETests/Infrastructure/Database/DatabaseHandler.cs
+++ bugs/Dima.E2ETests/Infrastructure/Database/DatabaseHandler.cs
@@ -7,6 +7,7 @@ public class DatabaseHandler
 {
     private static readonly MsSqlContainer Container = new MsSqlBuilder()
                             .WithPassword("somePassw0rd!")
+                            .WithImage("mcr.microsoft.com/windows/servercore:ltsc2019")
                             .WithPortBinding(37000, 1433)
                             .Build();
 
